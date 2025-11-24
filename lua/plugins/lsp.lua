c18bb05fa78922addc828813fc9d45b9e9190e6d
Zephyr Lathuy <zephyr.lathuy.zpl@gmail.com>
2025-11-24 13:57:35 +0100

    Reorganize LSP dependencies in lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,18 +3,17 @@ return {
     dependencies = {
         'williamboman/mason.nvim',
         'williamboman/mason-lspconfig.nvim',
-        'mfussenegger/nvim-jdtls', -- LSP pour Java (Spring Boot, etc.)
-        'hrsh7th/nvim-cmp',    -- auto-completion core
+        'mfussenegger/nvim-jdtls',
+        'hrsh7th/nvim-cmp',  
         'hrsh7th/cmp-buffer',
         'hrsh7th/cmp-path',
         'saadparwaiz1/cmp_luasnip',
         'hrsh7th/cmp-nvim-lsp',
         'hrsh7th/cmp-nvim-lua',
-        'L3MON4D3/LuaSnip',         -- snippets engine
-        'rafamadriz/friendly-snippets', -- snippets par défaut
+        'L3MON4D3/LuaSnip',      
+        'rafamadriz/friendly-snippets', 
     },
     config = function()
-        -- Appelle ton fichier lua/config/lsp.lua
         require('config.lsp').setup()
     end,
 }
